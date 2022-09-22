508e548438e038e339b38a522bb897e51d83ca0b
DevOpRohan <rohanvermadev@gmail.com>
2022-09-22 23:32:59 +0530

    apikey added

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const taskRoutes = require('./routes/taskRoutes');
 //AI setup
 const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
-    apiKey: config.aiKey
-  });
+  apiKey: config.aiKey
+});
 const preset = "\"\"\"\nThe following is a conversation with \"Vision\" an AI assistant. The assistant is helpful, creative, clever, and very friendly.\nVision can engage visually impaired people in conversations , Vision can help them in their learning, can entertain them and also acts as a mentor\n#Human: What is Atom?\n#AI: Atoms are the basic units of matter and the defining structure of elements. The term \"atom\" comes from the Greek word for indivisible, because it was once thought that atoms were the smallest things in the universe and could not be divided.\n\n#Human: Hi, How are you?\n#AI: I am Fine. How are you\n\nIf vision doesn't understand the context. Then it will ask for again\n\"\"\"\n#Human: ";
 const openai = new OpenAIApi(configuration);
 
@@ -21,21 +21,31 @@ app.use(bodyParser.json());
 app.use('/api', taskRoutes.routes);
 
 //aiGet
-app.get("/vision", async(req, res,next) => {
-    const {q} = req.query;
-    // console.log(q);
-    const response = await openai.createCompletion({
-      model: "text-davinci-002",
-      prompt: preset + q + "\n#AI: ",
-      temperature: 0,
-      max_tokens: 128,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0.6,
-      stop: ["#Human:", "#AI"],
-    });
-    // console.log(response.data.choices[0].text);
-    res.send(response.data.choices[0].text);
+app.get("/vision", async (req, res, next) => {
+  try {
+    const { q, ak } = req.query;
+    if (ak === "vision405706api") {
+      console.log(ak);
+      const response = await openai.createCompletion({
+        model: "text-davinci-002",
+        prompt: preset + q + "\n#AI: ",
+        temperature: 0,
+        max_tokens: 128,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0.6,
+        stop: ["#Human:", "#AI"],
+      });
+      res.send(response.data.choices[0].text);
+    }
+
+    else {
+      res.send("Invalid API Key");
+    }
+  }
+  catch (error) {
+    res.status(400).send(error.message);
+  }
 });
 
 app.listen(config.port, () => console.log(`Server is running on port ${config.port}`));
